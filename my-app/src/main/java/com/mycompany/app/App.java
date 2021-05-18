34e3a4efc5025852dfeaa15c08ce4916969b26be
NirGrossman1986 <82283999+NirGrossman1986@users.noreply.github.com>
2021-05-18 05:25:03 +0300

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,22 +8,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Nir!" );
 
-        System.out.println("A NullPointerException is a drag...");
-        Object myObject = null;
-        /**
-         * Checker prevents this from compiling...
-         */
-//        System.out.println("myObject: " + myObject.toString());
-        /**
-         * ... which is simply fantastic. It shows:
-         *
-         * error: [dereference.of.nullable] dereference of possibly-null
-         * reference myObject
-         *
-         * http://checkerframework.org
-         */
-        System.out.println("... but thankfully, Checker has our back: http://checkerframework.org");
+        int number;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number you want to check:");
+        number = scan.nextInt();
+        scan.close();
+        if(number > 0)
+        {
+            System.out.println(number+" is positive number");
+        }
+        else if(number < 0)
+        {
+            System.out.println(number+" is negative number");
+        }
+        else
+        {
+            System.out.println(number+" is neither positive nor negative");
+        }
     }
 }
