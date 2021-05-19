a250148e97c2664b5afc93177f16ea6348c23cd0
NirGrossman1986 <82283999+NirGrossman1986@users.noreply.github.com>
2021-05-19 14:29:01 +0300

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -9,23 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Nir!" );
-
-        int number;
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter the number you want to check:");
-        number = scan.nextInt();
-        scan.close();
-        if(number > 0)
-        {
-            System.out.println(number+" is positive number");
-        }
-        else if(number < 0)
-        {
-            System.out.println(number+" is negative number");
-        }
-        else
-        {
-            System.out.println(number+" is neither positive nor negative");
-        }
     }
 }
